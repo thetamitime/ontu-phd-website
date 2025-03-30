12dc894145ac78f9dd1679f1219a5a04bd2f801b
Tamila Zihura <z.m.tami@gmail.com>
2025-03-30 17:53:49 +0300

    fix fetch all documents and styling for titles and description

--- src/lib/api/documents.ts
+++ src/lib/api/documents.ts
@@ -2,9 +2,7 @@ import { notFound } from "next/navigation";
 import { Documents } from "@/lib/types";
 
 export async function getDocuments(type: string) {
-  const res = await fetch(`http://localhost:5124/api/documents?type=${type}`, {
-    cache: "force-cache",
-  });
+  const res = await fetch(`http://localhost:5124/api/documents?type=${type}`);
 
   const documents: Documents = await res.json();
   if (!documents) notFound();
--- src/ui/components/RequiredDocument.tsx
+++ src/ui/components/RequiredDocument.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface RequirementProps {
   number: number;
   title: string;
@@ -11,16 +13,38 @@ export const RequiredDocument = ({
   description,
   isLast = false,
 }: RequirementProps) => {
+  // avoid extra space if it starts with ","
+  const formattedDescription = description?.startsWith(",")
+    ? description
+    : ` ${description}`;
+
+  // regex for underlining "за наявністю" & "за необхідністю"
+  const highlightRegex = /(за наявністю|за необхідністю)/g;
+  const descriptionParts = formattedDescription.split(highlightRegex);
+
+  // split title into bold & non-bold parts based on parenthesis
+  const titleParts = title.split(/(\(.*?\))/g);
+
   return (
     <div className="flex items-center justify-between">
       <p className="text-lg font-semibold">{number}</p>
       <p className="w-[95%]">
-        <span className="font-bold">{title}</span>
-        {description
-          ? `${description}${isLast ? "." : ";"}`
-          : isLast
-            ? "."
-            : ";"}
+        {titleParts.map((part, index) => (
+          <span
+            key={index}
+            className={
+              part.startsWith("(") && part.endsWith(")") ? "" : "font-bold"
+            }
+          >
+            {part}
+            {/* avoid extra space */}
+            {titleParts[index + 1]?.startsWith(",") ? "" : " "}
+          </span>
+        ))}
+        {descriptionParts.map((part, index) =>
+          highlightRegex.test(part) ? <u key={index}>{part}</u> : part,
+        )}
+        {isLast ? "." : ";"}
       </p>
     </div>
   );
