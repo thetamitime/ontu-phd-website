ca2e62306605ac01318d31efcd227e605ae38e0f
Tamila Zihura <z.m.tami@gmail.com>
2025-04-01 03:53:56 +0300

    implement news api on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import { ProgramCardSmall } from "@/ui/components/ProgramCards";
 import { workers } from "../lib/json/Workers.json";
-import { news } from "../lib/json/News.json";
 import { FacultyCard } from "@/ui/components/FacultyCard";
 import { NewsCardLarge, NewsCardMedium } from "@/ui/components/NewsCards";
 import Link from "next/link";
 import { getProgramFields } from "@/lib/api/programs";
+import { getLatestNews } from "@/lib/api/news";
 
 export default async function Home() {
   const photoUrl =
     "https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  const [firstNewsCard, ...newsCards] = news;
 
   const programFields = await getProgramFields();
+  const latestNews = await getLatestNews();
+  const [firstLatestNews, ...restLatestNews] = latestNews;
 
   return (
     <main className="bg-base-200 min-h-full">
@@ -81,25 +82,29 @@ export default async function Home() {
           <div className="col grid w-full items-stretch gap-6 md:grid-cols-2 md:grid-rows-3">
             <div className="row-span-3">
               <NewsCardLarge
-                id={firstNewsCard.id}
-                category={firstNewsCard.category}
-                date={new Date(firstNewsCard.date)}
-                photo={firstNewsCard.photo}
-                title={firstNewsCard.title}
-                description={firstNewsCard.description}
-                link={firstNewsCard.link}
+                id={firstLatestNews.id}
+                title={firstLatestNews.title}
+                summary={firstLatestNews.summary}
+                mainTag={firstLatestNews.mainTag}
+                otherTags={firstLatestNews.otherTags}
+                date={firstLatestNews.date}
+                thumbnail={firstLatestNews.thumbnail}
+                photos={firstLatestNews.photos}
+                body={firstLatestNews.body}
               />
             </div>
-            {newsCards.map((newsCard) => (
+            {restLatestNews.map((newsCard) => (
               <NewsCardMedium
                 key={newsCard.id}
                 id={newsCard.id}
-                category={newsCard.category}
-                date={new Date(newsCard.date)}
-                photo={newsCard.photo}
                 title={newsCard.title}
-                description={newsCard.description}
-                link={newsCard.link}
+                summary={newsCard.summary}
+                mainTag={newsCard.mainTag}
+                otherTags={newsCard.otherTags}
+                date={newsCard.date}
+                thumbnail={newsCard.thumbnail}
+                photos={newsCard.photos}
+                body={newsCard.body}
               />
             ))}
           </div>
--- src/ui/components/NewsCards.tsx
+++ src/ui/components/NewsCards.tsx
@@ -1,35 +1,36 @@
 import Image from "next/image";
-import { News } from "@/lib/types";
+import { NewsSingular } from "@/lib/types";
 import Link from "next/link";
 
 export function NewsCardLarge({
   id,
-  category,
-  date,
-  photo,
   title,
-  description,
-  link,
-}: News) {
+  summary,
+  mainTag,
+  otherTags,
+  date,
+  thumbnail,
+  photos,
+  body,
+}: NewsSingular) {
+  const formattedDate = new Date(date).toLocaleString("uk-UA", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div
       className="card card-border border-base-300 bg-base-100 h-full"
       key={id}
     >
       <div className="card-body flex-none flex-row items-center justify-between py-4">
-        <div className="badge badge-soft badge-secondary"> {category} </div>
-        <div className="text-base-content/80 text-base">
-          {date.toLocaleString("uk-UA", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        </div>
+        <div className="badge badge-soft badge-secondary"> {mainTag} </div>
+        <div className="text-base-content/80 text-base">{formattedDate}</div>
       </div>
       <figure>
         <Image
-          src={photo}
-          alt={`Новина за тегом ${category} - ${title}`}
+          src={`/${thumbnail}`}
+          alt={`Новина за тегом ${mainTag} - ${title}`}
           className="size-full object-cover"
           width={1000}
           height={1000}
@@ -37,9 +38,12 @@ export function NewsCardLarge({
       </figure>
       <div className="card-body pb-0">
         <h4 className="card-title font-semibold"> {title} </h4>
-        <p className="text-base">{description}</p>
+        <p className="text-base">{summary}</p>
         <div className="card-actions">
-          <Link className="link link-hover link-primary py-4" href={link}>
+          <Link
+            className="link link-hover link-primary py-4"
+            href={`/news/${id}`}
+          >
             Продовжити читати
           </Link>
         </div>
@@ -50,29 +54,35 @@ export function NewsCardLarge({
 
 export function NewsCardMedium({
   id,
-  category,
-  date,
   title,
-  description,
-  link,
-}: News) {
+  summary,
+  mainTag,
+  otherTags,
+  date,
+  thumbnail,
+  photos,
+  body,
+}: NewsSingular) {
+  const formattedDate = new Date(date).toLocaleString("uk-UA", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <div className="card card-border border-base-300 bg-base-100" key={id}>
       <div className="card-body flex-none flex-row items-center justify-between py-4">
-        <div className="badge badge-soft badge-secondary"> {category} </div>
-        <div className="text-base-content/80 text-base">
-          {date.toLocaleString("uk-UA", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        </div>
+        <div className="badge badge-soft badge-secondary"> {mainTag} </div>
+        <div className="text-base-content/80 text-base">{formattedDate}</div>
       </div>
       <div className="card-body py-0">
         <h4 className="card-title font-semibold"> {title} </h4>
-        <p className="line-clamp-3 text-base">{description}</p>
+        <p className="line-clamp-3 text-base">{summary}</p>
         <div className="card-actions">
-          <Link className="link link-hover link-primary py-4" href={link}>
+          <Link
+            className="link link-hover link-primary py-4"
+            href={`/news/${id}`}
+          >
             Продовжити читати
           </Link>
         </div>
