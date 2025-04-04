755ffb9a7b6b0f6d31634a1e5248c0b0a045fd7c
Tamila Zihura <z.m.tami@gmail.com>
2025-04-04 16:16:23 +0300

    add defence api and types

---
+++ src/lib/api/defence.ts
@@ -0,0 +1,20 @@
+import { DefenseEvent } from "@/lib/types/defences";
+import { notFound } from "next/navigation";
+
+export const getAllDefences = async () => {
+  const res = await fetch("http://localhost:5124/api/defence");
+
+  const defences: DefenseEvent[] = await res.json();
+  if (!defences) notFound();
+  return defences;
+};
+
+export const getDefencesByDegree = async (degree: string) => {
+  const res = await fetch(
+    `http://localhost:5124/api/defence/degree?degree=${degree}`,
+  );
+
+  const defences: DefenseEvent[] = await res.json();
+  if (!defences) notFound();
+  return defences;
+};
