252fc70a2a504f968aa04afd7bfba3f0c06a0b75
Tamila Zihura <z.m.tami@gmail.com>
2025-04-15 18:44:00 +0300

    login working ver

--- src/app/(admin)/(auth)/login/page.tsx
+++ src/app/(admin)/(auth)/login/page.tsx
@@ -1,93 +1,84 @@
 "use client";
 
-import { useForm } from "@tanstack/react-form";
+import { createFormHook } from "@tanstack/react-form";
 import { useAuth } from "@/lib/utils/AuthProvider";
 import { useRouter } from "next/navigation";
+import { InputField, SubscribeButton } from "@/ui/components";
+import { fieldContext, formContext } from "@/lib/hooks/useFieldContext";
+import React from "react";
+import { LoginFormValues, loginSchema } from "@/lib/schemas/loginSchema";
+
+//=============Form Context=============
+const { useAppForm } = createFormHook({
+  fieldContext,
+  formContext,
+  fieldComponents: {
+    InputField,
+  },
+  formComponents: {
+    SubscribeButton,
+  },
+});
 
 export default function LoginPage() {
   const { login } = useAuth();
   const router = useRouter();
+  const [loginSuccess, setLoginSuccess] = React.useState(false);
 
-  const form = useForm({
+  const form = useAppForm({
     defaultValues: {
       email: "",
       password: "",
+    } as LoginFormValues,
+    validators: {
+      onSubmit: loginSchema,
+      onChange: loginSchema,
     },
-    onSubmit: async ({ value }) => {
+    onSubmit: ({ value }) => {
       try {
-        await login(value);
-        console.log(value);
-        router.push("/dashboard");
-      } catch (err) {
-        console.error("Login failed:", err);
+        login(value).then(() => {
+          setLoginSuccess(true);
+          router.push("/dashboard");
+        });
+      } catch (error) {
+        setLoginSuccess(false);
       }
     },
   });
 
   return (
-    <div>
-      <h1>Simple Form Example</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          form.handleSubmit();
-        }}
-      >
-        <div>
-          {/* A type-safe field component*/}
-          <form.Field name="email">
-            {(field) => {
-              return (
-                <>
-                  <label htmlFor={field.name}>Email:</label>
-                  <input
-                    id={field.name}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                </>
-              );
-            }}
-          </form.Field>
+    <main className="bg-base-200 flex h-screen items-center justify-center">
+      <div className="card card-border border-base-300 bg-base-100 h-fit w-84 p-5">
+        <div className="card-title">
+          <h1>Вхід для адміністраторів</h1>
         </div>
-        <div>
-          <form.Field name="password">
-            {(field) => {
-              return (
-                <>
-                  <label htmlFor={field.name}>Last Name:</label>
-                  <input
-                    id={field.name}
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                </>
-              );
-            }}
-          </form.Field>
-        </div>
-        <form.Subscribe
-          selector={(state) => [state.canSubmit, state.isSubmitting]}
-        >
-          {([canSubmit, isSubmitting]) => {
-            return (
-              <>
-                <button type="submit" disabled={!canSubmit}>
-                  {isSubmitting ? "..." : "Submit"}
-                </button>
-                <button type="reset" onClick={() => form.reset()}>
-                  Reset
-                </button>
-              </>
-            );
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            form.handleSubmit();
           }}
-        </form.Subscribe>
-      </form>
-    </div>
+        >
+          <div className="card-body px-0 py-1">
+            <form.AppField name="email">
+              {(field) => <field.InputField label="Пошта" />}
+            </form.AppField>
+
+            <form.AppField name="password">
+              {(field) => <field.InputField label="Пароль" />}
+            </form.AppField>
+          </div>
+
+          {!loginSuccess && !form.state.isPristine && (
+            <div className="alert alert-soft">Неправильно введено дані</div>
+          )}
+
+          <form.AppForm>
+            <form.SubscribeButton label="Вхід" className="btn-block mt-4" />
+          </form.AppForm>
+        </form>
+      </div>
+    </main>
   );
 }
--- src/ui/components/form/SubscribeButton.tsx
+++ src/ui/components/form/SubscribeButton.tsx
@@ -1,16 +1,27 @@
 import { useFormContext } from "@/lib/hooks/useFieldContext";
 
-export const SubscribeButton = ({ label }: { label: string }) => {
+export const SubscribeButton = ({
+  label,
+  className,
+  ...rest
+}: {
+  label: string;
+  className?: string;
+}) => {
   const form = useFormContext();
   return (
     <form.Subscribe selector={(state) => state.isSubmitting}>
       {(isSubmitting) => (
         <button
-          className="btn btn-soft btn-primary"
+          className={`${className} btn btn-soft btn-primary`}
           disabled={isSubmitting}
-          type="submit"
+          {...rest}
         >
-          {label}
+          {isSubmitting ? (
+            <span className="loading loading-dots loading-sm"></span>
+          ) : (
+            label
+          )}
         </button>
       )}
     </form.Subscribe>
